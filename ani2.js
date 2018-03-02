785ba7306d249cdb2a3ecda17ff7a2bc6f706aa4
Charles <cweng@stuy.edu>
2018-03-02 13:17:49 -0500

    works

--- ani2.js
+++ ani2.js
@@ -7,31 +7,28 @@
 
 
 /*
-  =======================================================================
+  ==============================================================================
                                   Variables/Initiation
-  =======================================================================
+  ==============================================================================
 */
 
 // the svg element
 var pic = document.getElementById('vimage');
 var frame = 0;
 // do circle or triangle?
-var circle;
-// how fast does the circle do its thing
-var circleSpeed = 21;
-// how do you like your square?
+var docircle;
+// circle things
+var circleSpeed = 2;
+// dvd things
 var squareW = 70;
 var squareH = 50;
-// how fast in each direction should it go?
-var velocity = [2,2];
-// logo stuffs
-var logo = new Image();
-logo.src = "./DVD.png";
+var velocity = [6,1];
+DVDsrc = "./DVD.png";
 
 /*
-  =======================================================================
+  ==============================================================================
                                   Functions
-  =======================================================================
+  ==============================================================================
 */
 
 // clear function
@@ -43,7 +40,7 @@ const clear = function(){
 
 // draw function
 const draw = function(){
-  if (circle){
+  if (docircle){
     drawC();
     return;
   }
@@ -64,40 +61,33 @@ const circle = function(x, y, r){
 // does the circle thing
 const drawC = function(){
   clear();
-  ctx.fillStyle ="#FF0000";
-  ctx.beginPath();
-  // draw the circle
-  ctx.moveTo(parseInt($('#slate')[0].width) / 2 , parseInt($('#slate')[0].height) / 2);
-  ctx.arc(parseInt($('#slate')[0].width) / 2 , parseInt($('#slate')[0].height) /  2, 100 + Math.abs(circleSpeed * frame % 100 - 50), 0, 2 * Math.PI);
-  ctx.stroke();
-  ctx.fill();
+  circle(parseInt(pic.getAttribute("width")), parseInt(pic.getAttribute("height")), 100 + Math.abs(circleSpeed * frame % 100 - 50));
   frame = window.requestAnimationFrame(drawC);
   console.log(frame);
 }
 
-// draw circle
+// draw dvd
 const dvd = function(x, y, w, h){
-  var cir = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  cir.setAttribute("cx", x);
-  cir.setAttribute("cy", y);
-  cir.setAttribute("r", r);
-  cir.setAttribute("fill", "red");
-  cir.setAttribute("stroke", "black");
-  cir = pic.appendChild(cir);
+  var svgimg = document.createElementNS('http://www.w3.org/2000/svg','image');
+  svgimg.setAttribute('height', squareH);
+  svgimg.setAttribute('width', squareW);
+  svgimg.setAttributeNS('http://www.w3.org/1999/xlink','href',DVDsrc);
+  svgimg.setAttribute('x', x);
+  svgimg.setAttribute('y', y);
+
+  pic.appendChild(svgimg);
 }
 
 // does the dvd thing
 const drawR = function(){
   clear();
-  ctx.fillStyle ="#FF0000";
   // upper left corner of square
-  ctx.beginPath();
-  var squareC = [0,0];
-  squareC[0] = Math.abs(velocity[0] * frame % ((parseInt($('#slate')[0].width) - squareW) * 2) - parseInt($('#slate')[0].width - squareW));
-  squareC[1] = Math.abs(velocity[1] * frame % ((parseInt($('#slate')[0].height) - squareH) * 2) - parseInt($('#slate')[0].height - squareH));
-  // ctx.fillRect(squareC[0], squareC[1], squareW, squareH);
-  ctx.drawImage(logo, squareC[0], squareC[1], squareW, squareH);
+  var square = [0,0];
+  square[0] = Math.abs(velocity[0] * frame % ((parseInt(pic.getAttribute("width")) - squareW) * 2) - parseInt(pic.getAttribute("width") - squareW));
+  square[1] = Math.abs(velocity[1] * frame % ((parseInt(pic.getAttribute("height")) - squareH) * 2) - parseInt(pic.getAttribute("height") - squareH));
+  dvd(square[0], square[1], squareW, squareH);
   frame = window.requestAnimationFrame(drawR);
+  console.log(square)
   console.log(frame);
 }
 
@@ -106,20 +96,20 @@ const stop = function(){
 }
 
 /*
-  =======================================================================
+  ==============================================================================
                                   Button Stuff
-  =======================================================================
+  ==============================================================================
 */
 
 // add event listenters
 document.getElementById("stop").addEventListener("click", stop);
 document.getElementById("start1").addEventListener("click", function(){
   stop();
-  circle = true;
+  docircle = true;
   draw();
 });
 document.getElementById("start2").addEventListener("click", function(){
   stop();
-  circle = false;
+  docircle = false;
   draw();
 });
